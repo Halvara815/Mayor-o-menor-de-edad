61e8904aa6a72d5b48dd7dc4069cc58d29c37a98
veget <veget@PANSHI>
2025-01-15 10:59:07 -0600

    Agregar archivos de proyecto.

---
+++ Mayor o menor de edad/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Mayor_o_menor_de_edad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.btnValidar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(67, 68);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(202, 20);
+            this.txtEdad.TabIndex = 0;
+            this.txtEdad.Text = "Ingrese su edad";
+            this.txtEdad.TextChanged += new System.EventHandler(this.txtEdad_TextChanged);
+            // 
+            // btnValidar
+            // 
+            this.btnValidar.Location = new System.Drawing.Point(80, 94);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(162, 40);
+            this.btnValidar.TabIndex = 1;
+            this.btnValidar.Text = "Validar";
+            this.btnValidar.UseVisualStyleBackColor = true;
+            this.btnValidar.Click += new System.EventHandler(this.btnValidar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(102, 150);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(12, 13);
+            this.lblResultado.TabIndex = 2;
+            this.lblResultado.Text = "/";
+            this.lblResultado.Click += new System.EventHandler(this.lblResultado_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 262);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnValidar);
+            this.Controls.Add(this.txtEdad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.Button btnValidar;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ Mayor o menor de edad/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mayor_o_menor_de_edad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+            // Obtener el texto del TextBox
+            string entrada = txtEdad.Text;
+            int edad;
+
+            // Validar que sea un número entero
+            if (int.TryParse(entrada, out edad) && edad >= 0)
+            {
+                if (edad >= 18)
+                {
+                    lblResultado.Text = "Eres mayor de edad.";
+                    lblResultado.ForeColor = System.Drawing.Color.Green;
+                }
+                else
+                {
+                    lblResultado.Text = "Eres menor de edad.";
+                    lblResultado.ForeColor = System.Drawing.Color.Blue;
+                }
+            }
+            else
+            {
+                lblResultado.Text = "Por favor, ingrese un número entero válido.";
+                lblResultado.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        private void txtEdad_TextChanged(object sender, EventArgs e)
+        {
+            string entrada = txtEdad.Text;
+
+        }
+
+        private void lblResultado_Click(object sender, EventArgs e)
+        {
+            lblResultado.Text = "Eres mayor de edad."; // Si es mayor de edad
+            lblResultado.ForeColor = System.Drawing.Color.Green;
+
+            lblResultado.Text = "Eres menor de edad."; // Si es menor de edad
+            lblResultado.ForeColor = System.Drawing.Color.Blue;
+
+            lblResultado.Text = "Por favor, ingrese un número entero válido."; // Si no es válido
+            lblResultado.ForeColor = System.Drawing.Color.Red;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+        
+    
+
